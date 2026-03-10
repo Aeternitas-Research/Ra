476fa680c2d360a216f53feda26c4be27efe8a73
Yifan Hu <224434986+yhu1729@users.noreply.github.com>
2026-03-10 16:40:49 -0400

    fix: set geometry type

--- src/mesh1d_constructor.cu
+++ src/mesh1d_constructor.cu
@@ -17,6 +17,8 @@ Mesh1D::Mesh1D() {}
 __host__
 Mesh1D::Mesh1D(const MeshConfig& config)
     : config(config) {
+  this->config.geometry.type = MeshElementType::Line;
+
   host.x.resize(2 * 1 * config.geometry.extent[0], thrust::no_init);
   host.f.resize(
     config.geometry.dof * config.geometry.extent[0], thrust::no_init);
