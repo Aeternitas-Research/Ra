3decd57205641cca35aa3b4e81037d975137a8db
Yifan Hu <224434986+yhu1729@users.noreply.github.com>
2026-03-11 17:52:39 -0400

    fix: cast to cudaError_t

--- src/error_invoke.cu
+++ src/error_invoke.cu
@@ -6,7 +6,7 @@ namespace ra {
 
 __host__ __device__ Error
 invoke_impl(const Error result, const char* file, const int line) {
-  cub::Debug(result.value, file, line);
+  cub::Debug(static_cast<cudaError_t>(result.value), file, line);
   if (result) {
     cuda::std::terminate();
   }
