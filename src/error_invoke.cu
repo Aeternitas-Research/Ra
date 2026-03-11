b7ad8f56bf1ab5ef2b999a71f772dc35a5901090
Yifan Hu <224434986+yhu1729@users.noreply.github.com>
2026-03-10 22:25:24 -0400

    feat: use cub::Debug

--- src/error_invoke.cu
+++ src/error_invoke.cu
@@ -1,16 +1,13 @@
 #include "ra/error.cuh"
+#include <cub/util_debug.cuh>
 #include <cuda/std/__exception/terminate.h>
-#include <iostream>
 
 namespace ra {
 
 __host__ __device__ Error
 invoke_impl(const Error result, const char* file, const int line) {
+  cub::Debug(result, file, line);
   if (result != cudaSuccess) {
-#if !defined(__CUDA_ARCH__)
-    std::cerr << "RA error " << result << " in " << file << ":" << line
-              << std::endl;
-#endif
     cuda::std::terminate();
   }
 
