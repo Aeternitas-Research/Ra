cb367205e5cb56bd8f3d03a986772d2cb32c3392
Yifan Hu <224434986+yhu1729@users.noreply.github.com>
2026-03-14 19:48:13 -0400

    test: add tests

--- test/error_constructor.cu
+++ test/error_constructor.cu
@@ -19,19 +19,3 @@ TEST_CASE("Error::Error", "[error]") {
   e6 = s5;
   e6 = std::move(e4);
 }
-
-TEST_CASE("Error::operator==", "[error]") {
-  using ra::Error;
-  using ra::ErrorCategory;
-  using ra::ErrorValue;
-
-  Error e1(ErrorValue::Success, ErrorCategory::Host);
-  Error e2(ErrorValue::Success, ErrorCategory::Host);
-  REQUIRE(e1 == e2);
-
-  e2.category = ErrorCategory::Device;
-  REQUIRE(e1 != e2);
-
-  Error e3(cudaSuccess);
-  REQUIRE(e2 == e3);
-}
