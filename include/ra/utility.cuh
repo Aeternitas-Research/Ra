241def81274137959c257493d72125e188a96ae4
Yifan Hu <224434986+yhu1729@users.noreply.github.com>
2026-03-13 20:11:09 -0400

    fix: add specifiers

--- include/ra/utility.cuh
+++ include/ra/utility.cuh
@@ -5,7 +5,7 @@
 namespace ra {
 
 template <typename T>
-constexpr typename cuda::std::underlying_type<T>::type
+__host__ __device__ constexpr typename cuda::std::underlying_type<T>::type
 to_underlying(T input) noexcept {
   return static_cast<typename cuda::std::underlying_type<T>::type>(input);
 }
