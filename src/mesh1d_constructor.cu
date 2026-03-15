0376f419d1965c09e4f3a8de7c56a57f4d1f09b0
Yifan Hu <224434986+yhu1729@users.noreply.github.com>
2026-03-14 20:28:01 -0400

    fix: check geometry

--- src/mesh1d_constructor.cu
+++ src/mesh1d_constructor.cu
@@ -9,15 +9,17 @@ Mesh1D::Mesh1D() : Mesh() {}
 Mesh1D::Mesh1D(const MeshConfig& in_config) : Mesh(in_config) {
   auto& config = this->config;
 
-  config.geometry.element.type = MeshElementType::Line;
-
   const auto extent = config.geometry.extent;
   const auto dof = config.geometry.element.dof;
-  const auto n = extent[0];
-  host.x.resize(2 * 1 * n, thrust::no_init);
-  host.f.resize(dof * n, thrust::no_init);
-  device.x.resize(2 * 1 * n, thrust::no_init);
-  device.f.resize(dof * n, thrust::no_init);
+  if (config.geometry.element.type == MeshElementType::Line) {
+    const auto n = extent[0];
+    host.x.resize(2 * 1 * n, thrust::no_init);
+    host.f.resize(dof * n, thrust::no_init);
+    device.x.resize(2 * 1 * n, thrust::no_init);
+    device.f.resize(dof * n, thrust::no_init);
+  } else {
+    return RA_ERROR(ErrorValue::InvalidGeometry);
+  }
 }
 
 } // namespace ra
