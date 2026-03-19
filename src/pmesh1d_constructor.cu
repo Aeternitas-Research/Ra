2244149612e708236de49e3e60764e62d3adc89c
Yifan Hu <224434986+yhu1729@users.noreply.github.com>
2026-03-19 19:06:06 -0400

    fix: remove invalid routine

--- src/pmesh1d_constructor.cu
+++ src/pmesh1d_constructor.cu
@@ -4,9 +4,7 @@ namespace ra {
 
 PMesh1D::~PMesh1D() {}
 
-PMesh1D::PMesh1D() : PMesh() {
-  this->config.geometry.element.type = MeshElementType::Line;
-}
+PMesh1D::PMesh1D() : PMesh() {}
 
 PMesh1D::PMesh1D(
   const int mpi_rank, const int* mpi_extent, const MeshConfig& config_global)
